c1aba627aacb71c9918383ef6275bdd3f40452ee
Hitohito1023 <ryotakinase1023@gmail.com>
2021-04-09 16:00:09 +0900

    [finish]Chapter6

--- src/chapter6/Main.java
+++ src/chapter6/Main.java
@@ -1,5 +1,35 @@
 package chapter6;
 
+interface Func1 {
+	boolean call(int x);
+}
+
+interface Func2 {
+	String call(boolean male, String name);
+}
+
 public class Main {
+//	public static int sub(int a, int b) {
+//		return a - b;
+//	}
+
+	public static void main(String[] args) {
+//		IntBinaryOperator func = Main::sub;
+//		int a = func.applyAsInt(5,  3);
+//		System.out.println("5-3は" + a);
+//
+//		IntBinaryOperator func = (int a, int b) -> { return a - b;};
+//		int a = func.applyAsInt(5, 3);
+//		System.out.println("5-3は" + a);
+
+		FuncList funclist = new FuncList();
+		Func1 f1 = FuncList::isOdd;
+		Func2 f2 = funclist::addNamePrefix;
+		System.out.println(f1.call(15));
+		System.out.println(f2.call(true, "Smith"));
+
+
+//
+	}
 
 }
