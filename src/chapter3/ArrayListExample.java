259cd3e0a1c43c390b85a934f76c8dc740ec8a51
Hitohito1023 <ryotakinase1023@gmail.com>
2021-04-12 18:38:04 +0900

    [start]chapter3

--- src/chapter3/ArrayListExample.java
+++ src/chapter3/ArrayListExample.java
@@ -1,8 +1,18 @@
 package chapter3;
 
+import java.util.ArrayList;
+
 public class ArrayListExample {
 	public static void main(String[] args) {
-		
+		ArrayList<String> names = new ArrayList<String>();
+
+		names.add("ryota");
+		names.add("taro");
+		names.add("jiro");
+
+		for(int i = 0; i < names.size(); i++) {
+			System.out.println(names.get(i));
+		}
 	}
 
 }
